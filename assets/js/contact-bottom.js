a7fd45fd9217653b752b2af8659d6d2e078cd108
Wulfhartus <thibaultmonneret@sfr.fr>
2026-04-19 00:05:46 +0200

    TD de géoalg, partie du bas avec un script js

---
+++ assets/js/contact-bottom.js
@@ -0,0 +1,54 @@
+/*
+	Script written by Thibault MONNERET for the template Escape Velocity by HTML5 UP.
+*/
+
+let monBas = document.getElementById('leBas');
+
+monBas.innerHTML = `
+<section id="footer" class="wrapper">
+	<div class="title">Contact</div>
+				<!-- Contact -->
+				<center> <p class="style1">Here is how to contact me. </p></center>
+					<section class="feature-list">
+					<div class="container"> 
+						<div class="row aln-center">
+							<div class="col-4 col-12-medium">
+								<section>
+									<h3 class="icon solid fa-home">Mailing Address</h3>
+									<p>
+										Thibault MONNERET<br />
+										ENS de Lyon site Monod<br />
+										UMPA UMR 5669 CNRS<br />
+										46, allée d'Italie<br />
+										69364 Lyon Cedex 07, France
+									</p>
+								</section>
+							</div>
+							<div class="col-4 col-12-medium">
+								<section>
+									<h3 class="icon solid fa-envelope">Email</h3>
+									<p>
+										firstname.name[at]ens-lyon.fr
+									</p>
+								</section>
+							</div>
+							<div class="col-4 col-12-medium">
+								<section>
+									<h3 class="icon solid fa-comment">Miscellaneous</h3>
+									<p>
+										<a href="https://www.umpa.ens-lyon.fr/recherche/equipe-de-theorie-et-des-nombres">Number theory group</a><br />
+										<a href="https://www.linkedin.com/in/thibault-monneret-474a3b131/">Linkedin page</a>
+									</p>
+								</section>
+							</div>
+						</div>
+					</div>
+					</section>
+		<div id="copyright">
+		<p> Website last updated on 04/2026.</p>
+		
+		<p>	This website template is <a href="https://html5up.net/uploads/demos/escape-velocity/">Escape Velocity</a>, released for
+		free under the <a href="http://html5up.net/license">Creative Commons Attribution 3.0 license</a>.
+		</div>
+</section>
+`;
